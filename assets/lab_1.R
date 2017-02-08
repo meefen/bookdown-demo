e3475a256c9b7c640b773150f45a3375625d735c
Bodong Chen <bodong.chen@gmail.com>
2017-02-08 13:29:25 -0600

    tweak wk 4 content

--- assets/lab_1.R
+++ assets/lab_1.R
@@ -1,9 +1,16 @@
+# Load igraph
 library(igraph)
 
-
-lesmis <- read.csv("assets/lesmis/lesmis.csv")
+# Read data
+lesmis <- read.csv("https://raw.githubusercontent.com/meefen/sna-ed/master/assets/lesmis/lesmis.csv")
+# check the first 6 rows
 head(lesmis)
 
-g <- graph_from_data_frame(lesmis)
+# Create a graph using the graph_from_data_frame function
+g <- graph.data.frame(lesmis)
+# g <- graph_from_data_frame(lesmis) # this line also works
+
+# Plot the graph
 plot(g)
+# make the graph a little prettier
 plot(g, edge.arrow.size=.2, vertex.label=NA, vertex.size=8)
