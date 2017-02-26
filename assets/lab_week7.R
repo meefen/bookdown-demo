c68265243608a75aadb81ab065c688b58771c460
Bodong Chen <bodong.chen@gmail.com>
2017-02-25 23:14:07 -0600

    init

---
+++ assets/lab_week7.R
@@ -0,0 +1,32 @@
+### Lab - Week 7
+
+## Read data from Excel
+library(readxl)
+cscw = read_excel("assets/cscw17.xlsx")
+cscw_mentions = cscw[cscw$Relationship == "Mentions", ]
+
+## Create graph
+library(igraph)
+g = graph_from_data_frame(cscw, directed = TRUE)
+get.edge.attribute(g)
+
+## Plot graph
+plot(g)
+
+## Network-level
+graph.density(g)
+diameter(g)
+cohesion(g)
+
+## Node-level
+degree(g)
+betweenness(g)
+
+## Components
+components(g)
+clusters(g)
+
+## Cliques
+cliques(g)
+largest.cliques(g)
+
